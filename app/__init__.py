6466cd80a744606c327cbd38dda0baad5f74d7c6
Parth <pjpanot260305@gmail.com>
2025-10-26 21:36:32 +0530

    added cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 # /secure-file-sender/app/__init__.py
 from flask import Flask
 from .crypto import generate_keys
-
+from flask_cors import CORS
 def create_app():
     # Generate the server's keys on startup
     generate_keys()
     
     app = Flask(__name__)
-
+    CORS(app)
     # Register the routes from routes.py
     with app.app_context():
         from . import routes
